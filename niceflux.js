c975c909d3f6df6c7516d57222806dfec872ee57
max302 <max302@gmail.com>
2018-04-14 17:05:17 -0400

    Added per-algo worker polling and made it async after global stats poll.

--- niceflux.js
+++ niceflux.js
@@ -20,86 +20,71 @@ var conf = {
 }
 // End Configuration
 
-// Schema information
-/*
-conf.influx.schema = [
-  {
-    measurement : "worker",
-    tags  : "name",
-    fields : {
-      // Fields listed in stats.provider.workers provider in Nicehash API
-      addr : FieldType.STRING,
-      a : FieldType.FLOAT,
-      rs : FieldType.FLOAT,
-      contime : FieldType.INTEGER,
-      xnsub : FieldType.BOOL,
-      diff : FieldType.FLOAT,
-      loc : FieldType.STRING,
-    }
-  },
-  {
-    measurement : "balance",
-    tags  : "address",
-    fields : {
-      // Fields listed in stats.provider.ex. Not all fields are used.
-      balance : FieldType.FLOAT,
-      profitability : FieldType.FLOAT
-  }
-}]
-*/
-
 var algo_info = [   // Array index corresponds to algo as documented here: https://www.nicehash.com/doc-api
-  { name : "Scrypt", suffix : ""},
-  { name : "SHA256", suffix : ""},
-  { name : "ScryptNf", suffix : ""},
-  { name : "X11", suffix : ""},
-  { name : "X13", suffix : ""},
-  { name : "Keccak", suffix : ""},
-  { name : "X15", suffix : ""},
-  { name : "Nist5", suffix : ""},
-  { name : "NeoScrypt", suffix : ""},
-  { name : "Lyra2RE", suffix : ""},
-  { name : "WhirlpoolX", suffix : ""},
-  { name : "Qubit", suffix : ""},
-  { name : "Quark", suffix : ""},
-  { name : "Axiom", suffix : ""},
-  { name : "Lyra2REv2", suffix : ""},
-  { name : "ScryptJaneNf16", suffix : ""},
-  { name : "Blake256r8", suffix : ""},
-  { name : "Blake256r8vnl", suffix : ""},
-  { name : "Hodl", suffix : ""},
-  { name : "DaggerHashimoto", suffix : ""},
-  { name : "Decred", suffix : ""},
-  { name : "CryptoNight", suffix : ""},
-  { name : "Lbry", suffix : ""},
-  { name : "Equihash", suffix : ""},
-  { name : "Pascal", suffix : ""},
-  { name : "X11Gost", suffix : ""},
-  { name : "Sia", suffix : ""},
-  { name : "Blake2s", suffix : ""},
-  { name : "Skunk", suffix : ""},
-  { name : "CryptoNightV7", suffix : ""},
-]
+  { name : "Scrypt", suffix : "", active : 0 },
+  { name : "SHA256", suffix : "", active : 0 },
+  { name : "ScryptNf", suffix : "", active : 0 },
+  { name : "X11", suffix : "", active : 0 },
+  { name : "X13", suffix : "", active : 0 },
+  { name : "Keccak", suffix : "", active : 0 },
+  { name : "X15", suffix : "", active : 0 },
+  { name : "Nist5", suffix : "", active : 0 },
+  { name : "NeoScrypt", suffix : "", active : 0 },
+  { name : "Lyra2RE", suffix : "", active : 0 },
+  { name : "WhirlpoolX", suffix : "", active : 0 },
+  { name : "Qubit", suffix : "", active : 0 },
+  { name : "Quark", suffix : "", active : 0 },
+  { name : "Axiom", suffix : "", active : 0 },
+  { name : "Lyra2REv2", suffix : "", active : 0 },
+  { name : "ScryptJaneNf16", suffix : "", active : 0 },
+  { name : "Blake256r8", suffix : "", active : 0 },
+  { name : "Blake256r14", suffix : "", active : 0 },
+  { name : "Blake256r8vnl", suffix : "", active : 0 },
+  { name : "Hodl", suffix : "", active : 0 },
+  { name : "DaggerHashimoto", suffix : "", active : 0 },
+  { name : "Decred", suffix : "", active : 0 },
+  { name : "CryptoNight", suffix : "", active : 0 },
+  { name : "Lbry", suffix : "", active : 0 },
+  { name : "Equihash", suffix : "", active : 0 },
+  { name : "Pascal", suffix : "", active : 0 },
+  { name : "X11Gost", suffix : "", active : 0 },
+  { name : "Sia", suffix : "", active : 0 },
+  { name : "Blake2s", suffix : "", active : 0 },
+  { name : "Skunk", suffix : "", active : 0 },
+  { name : "CryptoNightV7", suffix : "", active : 0 },
+];
 
 //Init Influxdb instance
 const influx = new Influx.InfluxDB(conf.influx);
 
-function get_data(method, callback) {
-  var client = restify.createJsonClient({ url : "https://api.nicehash.com", query : { method : method, addr : conf.nicehash.addr }});
-  client.get("/api", function(err, req, res, obj) {
-    assert.ifError(err);
-    callback(obj);
-    return 0;
+function get_data(method,algo,callback) {
+  var args = { url : "https://api.nicehash.com", query : { method : method, addr : conf.nicehash.addr}}
+  if (algo){
+    args.query.algo = algo;
+  }
+  var client  = restify.createJsonClient(args);
+  return new Promise(function(resolve, reject) {
+    client.get("/api", function(err, req, res, obj) {
+      if (err){
+        reject(err);
+      }
+      else {
+        callback(obj);  // Is callback called in async manner?  Seems to work, but TODO
+        resolve(1);
+      }
+
+    });
   });
 }
 
 
-function log_workers(results) {
+function log_worker(results) {
   results.result.workers.forEach(function(i) {
     //Normalized values to the proper types. Maybe this can be done with the influx schema?
     speed_object = Object.keys(i[1])[0];
     speed = Object.values(i[1])[0];
     console.log("Worker " + i[0] + ": " + speed + " " + speed_object);
+
     influx.writePoints([
       {
         measurement: 'worker',
@@ -112,7 +97,7 @@ function log_workers(results) {
           loc : i[5]
         },
       }
-    ])
+    ]);
   } );
 
 }
@@ -140,16 +125,22 @@ function log_global(results) {
         },
       }
     ])
+
+    algo_info[i.algo].active = 1;
   })
   console.log("--------------------------")
 }
 
-  get_data("stats.provider.workers", log_workers);
-  get_data("stats.provider.ex", log_global);
-
-/*
-
-printres(var results) {
-  console.log(JSON.stringify(results));
+function get_active(){
+  algo_info.forEach(function(algo,i){
+    if (algo.active == 1) {
+      get_data("stats.provider.workers", i , log_worker);
+    }
+  });
 }
-*/
+
+const asyncloop  = async () => {
+  await get_data("stats.provider.ex","", log_global);
+  get_active();
+}
+asyncloop();
---
+++ schema.js
@@ -0,0 +1,30 @@
+// Schema information
+
+// Currently unused. TODO
+
+/*
+conf.influx.schema = [
+  {
+    measurement : "worker",
+    tags  : "name",
+    fields : {
+      // Fields listed in stats.provider.workers provider in Nicehash API
+      addr : FieldType.STRING,
+      a : FieldType.FLOAT,
+      rs : FieldType.FLOAT,
+      contime : FieldType.INTEGER,
+      xnsub : FieldType.BOOL,
+      diff : FieldType.FLOAT,
+      loc : FieldType.STRING,
+    }
+  },
+  {
+    measurement : "balance",
+    tags  : "address",
+    fields : {
+      // Fields listed in stats.provider.ex. Not all fields are used.
+      balance : FieldType.FLOAT,
+      profitability : FieldType.FLOAT
+  }
+}]
+*/
